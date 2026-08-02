d57885e235421b86b69733816d107076eec1c429
JezCH <choijanggyu@gmail.com>
2026-08-02 09:16:36 +0900

    Load supplemental person ingestion

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ window.ATLAS_CONFIG = {
 
 window.addEventListener("load", () => {
   const files = [
+    "ingest-supplement.js",
     "person-locales.js",
     "person-locales-supplement.js",
     "status-summary.js",
