65be4ad2aa07d08b8a33ec0a2a9b8ec03f026ef3
JezCH <choijanggyu@gmail.com>
2026-08-02 18:36:12 +0900

    Add Korean labels for supplement 4

---
+++ person-locales-supplement-4.js
@@ -0,0 +1,49 @@
+window.ATLAS_PERSON_LOCALES = window.ATLAS_PERSON_LOCALES || {};
+window.ATLAS_POLITIC_LOCALES = window.ATLAS_POLITIC_LOCALES || {};
+
+Object.assign(window.ATLAS_PERSON_LOCALES, {
+  "Confucius": "공자",
+  "Jesus": "예수",
+  "Gautama Buddha": "석가모니",
+  "Muhammad": "무함마드",
+  "Plato": "플라톤",
+  "Toyotomi Hideyoshi": "도요토미 히데요시",
+  "Gilbert du Motier, Marquis de Lafayette": "라파예트 후작",
+  "Lakshmibai": "락슈미 바이",
+  "Niccolo Machiavelli": "니콜로 마키아벨리",
+  "Benjamin Franklin": "벤저민 프랭클린",
+  "Sayyida al-Hurra": "사이이다 알 후라",
+  "Amina": "아미나",
+  "Edward Teach": "에드워드 티치",
+  "Ada Lovelace": "에이다 러브레이스",
+  "Catherine II": "예카테리나 2세",
+  "Ibn Battuta": "이븐 바투타",
+  "Isabella I": "이사벨 1세",
+  "Trung Trac": "쯩짝",
+  "Tecumseh": "테쿰세",
+  "Harriet Tubman": "해리엇 터브먼",
+  "Jose Rizal": "호세 리살"
+});
+
+Object.assign(window.ATLAS_POLITIC_LOCALES, {
+  "State of Lu": "노나라",
+  "Roman Judaea": "로마령 유대",
+  "Shakya": "샤카족",
+  "Medina": "메디나",
+  "Athens": "아테네",
+  "Japan": "일본",
+  "Kingdom of France": "프랑스 왕국",
+  "Jhansi": "잔시",
+  "Republic of Florence": "피렌체 공화국",
+  "United States": "미국",
+  "Tetouan": "테투안",
+  "Zazzau": "자자우",
+  "Republic of Pirates": "해적 공화국",
+  "United Kingdom": "영국",
+  "Russian Empire": "러시아 제국",
+  "Marinid Sultanate": "마리니드 술탄국",
+  "Crown of Castile": "카스티야 왕국",
+  "Trung Sisters' Realm": "쯩 자매 정권",
+  "Shawnee": "쇼니족",
+  "Captaincy General of the Philippines": "필리핀 총독령"
+});
