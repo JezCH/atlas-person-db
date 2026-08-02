15e942870123870d2502b35c076f5130882105d6
JezCH <choijanggyu@gmail.com>
2026-08-02 09:56:03 +0900

    Add Korean labels for Askia Muhammad

---
+++ person-locales-supplement-2.js
@@ -0,0 +1,27 @@
+(() => {
+  "use strict";
+
+  const map = {
+    "Askia Muhammad": "아스키아 무함마드",
+    "Songhai Empire": "송가이 제국"
+  };
+
+  function apply(root = document) {
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+    const nodes = [];
+    while (walker.nextNode()) nodes.push(walker.currentNode);
+    nodes.forEach((node) => {
+      const value = node.nodeValue?.trim();
+      if (value && map[value]) node.nodeValue = node.nodeValue.replace(value, map[value]);
+    });
+  }
+
+  const observer = new MutationObserver(() => apply(document));
+  function start() {
+    apply(document);
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+  if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", start, { once: true });
+  else start();
+})();
