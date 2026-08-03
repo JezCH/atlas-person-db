c993ae0d51a303d916eaf1f5a16d689317ec9172
JezCH <choijanggyu@gmail.com>
2026-08-03 23:57:37 +0900

    Register supplement 10 in canonical manifest

--- atlas-canonical-data.js
+++ atlas-canonical-data.js
@@ -12,6 +12,7 @@
     "./pending-records-supplement-7.json",
     "./pending-records-supplement-8.json",
     "./pending-records-supplement-9.json",
+    "./pending-records-supplement-10.json",
     "./pending-records-corrections.json"
   ]);
 
