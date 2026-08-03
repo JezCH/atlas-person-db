251b2fe99ab4bf587dd4c216aa14f28279cd69a0
JezCH <choijanggyu@gmail.com>
2026-08-03 23:23:07 +0900

    Include supplements 7-9 in live summary

--- status-summary.js
+++ status-summary.js
@@ -96,6 +96,9 @@
       "./pending-records-supplement-4.json",
       "./pending-records-supplement-5.json",
       "./pending-records-supplement-6.json",
+      "./pending-records-supplement-7.json",
+      "./pending-records-supplement-8.json",
+      "./pending-records-supplement-9.json",
       "./pending-records-corrections.json"
     ];
     const [datasets, nonTimeline] = await Promise.all([
