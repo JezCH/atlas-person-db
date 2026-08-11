0e164926fa96bbd789661e9345e1d1fc0cfcea30
JezCH <choijanggyu@gmail.com>
2026-08-11 23:14:36 +0900

    feat(correction): add correction migration registry

---
+++ server/atlas-correction-migrations.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const CORRECTION_MIGRATION_PATHS = Object.freeze([
+  path.resolve(__dirname, "../db/migrations/20260811_correction_manifest_runs.sql")
+]);
+
+function readCorrectionMigrations({ readFile = fs.readFileSync } = {}) {
+  return CORRECTION_MIGRATION_PATHS.map((migrationPath) => ({
+    path: migrationPath,
+    sql: readFile(migrationPath, "utf8")
+  }));
+}
+
+async function applyCorrectionMigrations(client, { readFile } = {}) {
+  if (!client || typeof client.query !== "function") throw new Error("PostgreSQL client is required");
+  const migrations = readCorrectionMigrations({ ...(readFile ? { readFile } : {}) });
+  for (const migration of migrations) await client.query(migration.sql);
+  return Object.freeze({ applied: migrations.map((migration) => path.basename(migration.path)) });
+}
+
+module.exports = Object.freeze({
+  CORRECTION_MIGRATION_PATHS,
+  readCorrectionMigrations,
+  applyCorrectionMigrations
+});
