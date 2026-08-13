6dd9d9c3f06ae55df7039b7b8d66c1519838abfb
JezCH <choijanggyu@gmail.com>
2026-08-13 13:55:13 +0900

    Chain P6 Batch 14 verifier

--- scripts/verify-stage2-p5-polity-authoring-batch3.mjs
+++ scripts/verify-stage2-p5-polity-authoring-batch3.mjs
@@ -68,3 +68,4 @@ await import('./verify-stage2-correction-v2-prebinding-batch10.mjs');
 await import('./verify-stage2-correction-v2-prebinding-batch11.mjs');
 await import('./verify-stage2-correction-v2-prebinding-batch12.mjs');
 await import('./verify-stage2-correction-v2-prebinding-batch13.mjs');
+await import('./verify-stage2-correction-v2-prebinding-batch14.mjs');
