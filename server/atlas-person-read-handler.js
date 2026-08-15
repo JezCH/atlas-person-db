86e11a20f2194a8f3cabdefa251ac3ba3946d908
JezCH <choijanggyu@gmail.com>
2026-08-15 21:35:08 +0900

    Fix R2 UUID validation regression

--- server/atlas-person-read-handler.js
+++ server/atlas-person-read-handler.js
@@ -4,7 +4,7 @@ const { readPersons, readPersonDetail } = require("./atlas-person-read-service.j
 const { readPersonListSemantics } = require("./atlas-person-list-semantic-service.js");
 const { requireDatabaseUrl, sendJson } = require("./atlas-normalized-read-handler.js");
 
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 function personIdFromRequest(req) {
   const direct = req?.query?.person_id;
