a79aab1e39cfcc7d5cd4cd79dfc1d3b3c93b88f0
JezCH <choijanggyu@gmail.com>
2026-08-16 23:42:19 +0900

    Expose bounded Korean localization repair endpoint

---
+++ api/atlas-authoritative-ko-repair.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const { createAuthoritativeKoRepairHandler } = require("../server/atlas-authoritative-ko-repair-handler.js");
+
+module.exports = createAuthoritativeKoRepairHandler();
