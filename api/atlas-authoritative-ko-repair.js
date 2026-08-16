4ac18664047ca45988cd9e6c16f361e39b890761
JezCH <choijanggyu@gmail.com>
2026-08-17 00:01:15 +0900

    Remove extra Vercel function for Korean repair

--- api/atlas-authoritative-ko-repair.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const { createAuthoritativeKoRepairHandler } = require("../server/atlas-authoritative-ko-repair-handler.js");
-
-module.exports = createAuthoritativeKoRepairHandler();
