dffaf28e1e4134d8b7bca0f6705a76f689db9e2d
JezCH <choijanggyu@gmail.com>
2026-08-19 19:49:58 +0900

    Log sanitized human authoring failure codes

--- api/atlas-authoring.js
+++ api/atlas-authoring.js
@@ -3,4 +3,25 @@
 const { createPostgresClient } = require("../server/atlas-postgres-client.js");
 const { createHumanAuthoringHandler } = require("../server/atlas-human-authoring-handler.js");
 
-module.exports = createHumanAuthoringHandler({ clientFactory:createPostgresClient });
+const handler = createHumanAuthoringHandler({ clientFactory:createPostgresClient });
+
+module.exports = async function loggedHumanAuthoringHandler(req, res) {
+  const originalEnd = res.end.bind(res);
+  res.end = function loggedEnd(body, ...args) {
+    if (Number(res.statusCode) >= 400) {
+      try {
+        const failure = JSON.parse(String(body || "{}"));
+        console.error("ATLAS_HUMAN_AUTHORING_FAILURE", JSON.stringify({
+          status:Number(res.statusCode),
+          code:failure?.code || null,
+          failed_index:Number.isInteger(failure?.failed_index) ? failure.failed_index : null,
+          manifest_path:failure?.manifest_path || null
+        }));
+      } catch {
+        console.error("ATLAS_HUMAN_AUTHORING_FAILURE", JSON.stringify({ status:Number(res.statusCode), code:"UNPARSEABLE_ERROR_RESPONSE" }));
+      }
+    }
+    return originalEnd(body, ...args);
+  };
+  return handler(req, res);
+};
