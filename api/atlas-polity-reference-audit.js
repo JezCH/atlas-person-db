89f2e882bc805bb9913c0785d7b8849480f0d323
JezCH <choijanggyu@gmail.com>
2026-08-21 06:52:05 +0900

    Keep ATLAS API count within Hobby limit

--- api/atlas-polity-reference-audit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const { createPolityReferenceAuditHandler } = require("../server/atlas-polity-reference-audit-handler.js");
-
-module.exports = createPolityReferenceAuditHandler();
-module.exports.createPolityReferenceAuditHandler = createPolityReferenceAuditHandler;
