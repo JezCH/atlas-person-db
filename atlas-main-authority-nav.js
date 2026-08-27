7fcedf55907fe7f38f0c004b0a2f376ea11d5bbf
JezCH <choijanggyu@gmail.com>
2026-08-27 11:03:37 +0900

    Refresh spacetime renderer cache key

--- atlas-main-authority-nav.js
+++ atlas-main-authority-nav.js
@@ -140,7 +140,7 @@
     if (spacetimeAssetsPromise) return spacetimeAssetsPromise;
     appendStylesheetOnce("./atlas-person-spacetime-view.css?v=20260827-single-result-nav");
     spacetimeAssetsPromise = loadScriptOnce("./atlas-person-spacetime-model.js?v=20260817-spacetime-v2", () => Boolean(window.ATLAS_PERSON_SPACETIME_MODEL))
-      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260827-rail-accessible-name", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
+      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260827-search-ime", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
       .then(() => window.ATLAS_PERSON_SPACETIME_VIEW)
       .catch((error) => {
         spacetimeAssetsPromise = null;
