53ee786684040ddae8b078b3395f339474fd6577
JezCH <choijanggyu@gmail.com>
2026-09-03 23:06:10 +0900

    Load full-name spacetime assets

--- atlas-main-authority-nav.js
+++ atlas-main-authority-nav.js
@@ -138,9 +138,9 @@
   function ensureSpacetimeAssets() {
     if (window.ATLAS_PERSON_SPACETIME_VIEW) return Promise.resolve(window.ATLAS_PERSON_SPACETIME_VIEW);
     if (spacetimeAssetsPromise) return spacetimeAssetsPromise;
-    appendStylesheetOnce("./atlas-person-spacetime-view.css?v=20260903-taxonomy-r2");
-    spacetimeAssetsPromise = loadScriptOnce("./atlas-person-spacetime-model.js?v=20260903-taxonomy-r2", () => Boolean(window.ATLAS_PERSON_SPACETIME_MODEL))
-      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260903-taxonomy-r2", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
+    appendStylesheetOnce("./atlas-person-spacetime-view.css?v=20260903-full-person-labels");
+    spacetimeAssetsPromise = loadScriptOnce("./atlas-person-spacetime-model.js?v=20260903-full-person-labels", () => Boolean(window.ATLAS_PERSON_SPACETIME_MODEL))
+      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260903-full-person-labels", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
       .then(() => window.ATLAS_PERSON_SPACETIME_VIEW)
       .catch((error) => {
         spacetimeAssetsPromise = null;
