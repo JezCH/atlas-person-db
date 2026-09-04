ac77d186f33ddea7513fefdd07a9da100cd0b68d
JezCH <choijanggyu@gmail.com>
2026-09-04 19:13:28 +0900

    Consolidate person domain onto mutation API

--- api/atlas-person-domain.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const { createPostgresClient } = require("../server/atlas-postgres-client.js");
-const { createPersonDomainHandler } = require("../server/atlas-person-domain-handler.js");
-
-module.exports = createPersonDomainHandler({ clientFactory:createPostgresClient });
