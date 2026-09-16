91252e8fffad8c08cd9f9613fa77a2109185cf51
JezCH <choijanggyu@gmail.com>
2026-09-16 23:02:04 +0900

    Spatial r4: materialize final 14 reviewed static placements (#1114)
    Materialize the remaining 14 already-reviewed static placements from the 61-polity macro-only audit and extend the r4 compiler regression contract to all 51 reviewed static UUID mappings.

--- tests/spatial-taxonomy-r4-compiler.test.mjs
+++ tests/spatial-taxonomy-r4-compiler.test.mjs
@@ -16,6 +16,7 @@ const shardDir = 'spatial/reviewed-bindings/shards';
 const migrationDir = 'spatial/taxonomy-migrations';
 
 const expected = new Map([
+  ['00ec4b0c-6002-5791-825c-43465632102d', 'north-india-ganges'],
   ['031bdca0-d5e8-51e8-9f59-2db5ee51c55f', 'mainland-southeast-asia'],
   ['0d7a50f2-e6ea-4a02-aedc-67c9b1bd9bb3', 'western-siberia'],
   ['0eec74a4-a2a4-43f3-b347-54370b94da14', 'himalayas'],
@@ -25,7 +26,9 @@ const expected = new Map([
   ['1fa78018-e3af-55c3-8b8e-1bf7ad1c4b08', 'north-america'],
   ['264b069b-994f-4a2c-8af1-7331c631a376', 'eastern-europe'],
   ['29a81993-13e5-4bca-bf9b-d7421913dcd2', 'western-siberia'],
+  ['4c3e50cb-d76c-4b7c-b611-222849404142', 'eastern-central-asia-steppe'],
   ['4d4eee6d-389b-4ab6-8bca-fda9314ff796', 'eastern-europe'],
+  ['5247c9c0-ca50-452b-9e5a-d7e5585f096d', 'western-europe'],
   ['54433b65-1eb1-5c41-b440-6c4de8e18417', 'japan'],
   ['5ee9928c-e6f4-4107-b078-1ae908db9beb', 'tibetan-plateau'],
   ['67ea837b-9ff6-4ade-ba49-00e73cf0b4b3', 'north-america'],
@@ -33,16 +36,26 @@ const expected = new Map([
   ['7518462f-9769-413f-a245-cc3825b45d17', 'himalayas'],
   ['769e6a5e-b49b-5fec-9a2b-fa78999763a1', 'australasia'],
   ['80810807-0abb-5255-b40c-1945f4073eb1', 'north-india-ganges'],
+  ['82203ec1-60b7-5f5c-bab2-9b8df1fd8040', 'north-india-ganges'],
   ['8382a731-65c6-4479-8fb1-553141313e2b', 'western-siberia'],
   ['84e6365e-cd43-42cd-874e-3c517cd8243a', 'western-siberia'],
+  ['851be7b2-6364-42fd-ad45-ec89db553612', 'maritime-southeast-asia'],
+  ['86e4798b-85fa-4a69-838f-7b75f87bc997', 'north-india-ganges'],
   ['8b335f26-bbc3-48db-9af7-66d29eaa8be5', 'eastern-siberia-far-east'],
+  ['8d91aa0d-7a55-42d5-a2a8-919b91cfd9c3', 'central-europe'],
+  ['8e0892b7-22f9-465c-b2c6-1f00191b7a7f', 'eastern-siberia-far-east'],
   ['8ed716e1-44b9-4cdd-8efd-ee19a4b63f40', 'western-siberia'],
   ['95fc9762-58e2-4045-aba4-d555ca0ebd66', 'tibetan-plateau'],
   ['9639b3f0-9b94-42d3-98b9-273c7a601def', 'tibetan-plateau'],
   ['a127a8aa-6e17-5045-9bd6-de4df7b53390', 'central-africa'],
+  ['b0cc75d1-346a-45ac-9a8d-e7155fa3dbeb', 'iranian-plateau'],
+  ['b787ca69-b1d0-4724-8fcb-40a45567380e', 'deccan-south-india'],
   ['b82ec632-5365-5f62-8aec-550548d63f83', 'arabia'],
   ['ba7a7e2d-3ba5-5a54-a98f-88294d3429f7', 'western-europe'],
+  ['c3d1b6ad-742e-4abe-8ae4-5baa21a9fc05', 'central-europe'],
   ['c591bebb-90a3-5a96-90c5-9870ddd7f637', 'north-america'],
+  ['cbe32fde-c91c-4071-b360-bdc77e27d47d', 'western-europe'],
+  ['cc1a203a-c6bc-5011-a6d2-b3fd6a9b3c53', 'nile-valley'],
   ['cfe1faef-3a82-5b6c-b542-cff2bd667c6c', 'western-europe'],
   ['d16ef45f-9721-4eaf-809f-c2ccdc499fb4', 'manchuria'],
   ['d4c6c00a-5309-48b9-aaaf-80d43ca3700a', 'central-europe'],
@@ -53,6 +66,7 @@ const expected = new Map([
   ['e3ca4f3c-3504-4830-b141-f4cdd0157763', 'himalayas'],
   ['efc86adb-7fc7-5efe-9c4d-7cd8e224890f', 'northern-europe'],
   ['f67af5a4-a2f5-492d-9220-92fe401bc167', 'western-siberia'],
+  ['fc5e9dc5-f431-530a-bb1e-15539190f13c', 'britain-ireland'],
 ]);
 
 test('r4 reviewed static migration manifest matches the current reviewed spatial source exactly', () => {
