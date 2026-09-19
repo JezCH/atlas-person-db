9f0df8ca25bc38c0cb7e1bd52a607a268d102d99
JezCH <choijanggyu@gmail.com>
2026-09-19 10:29:28 +0900

    Add bounded OIDC policy for one-off person hard delete

---
+++ server/atlas-person-hard-delete-github-oidc.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const {
+  verifyGitHubActionsOidcWithPolicy,
+  verifyTrustClaimsWithPolicy,
+  EXPECTED_REPOSITORY,
+  EXPECTED_REPOSITORY_ID,
+  EXPECTED_REF
+} = require("./atlas-github-oidc.js");
+
+const EXPECTED_AUDIENCE = "atlas-person-db-person-hard-delete";
+const EXPECTED_WORKFLOW_REF = "JezCH/atlas-person-db/.github/workflows/atlas-oneoff-bathory-hard-delete.yml@refs/heads/main";
+const PERSON_HARD_DELETE_POLICY = Object.freeze({
+  audience: EXPECTED_AUDIENCE,
+  repository: EXPECTED_REPOSITORY,
+  repositoryId: EXPECTED_REPOSITORY_ID,
+  ref: EXPECTED_REF,
+  workflowRef: EXPECTED_WORKFLOW_REF,
+  environment: "production",
+  allowedEvents: new Set(["push"])
+});
+
+function verifyPersonHardDeleteTrustClaims(payload, expectedSha) {
+  return verifyTrustClaimsWithPolicy(payload, expectedSha, PERSON_HARD_DELETE_POLICY);
+}
+
+async function verifyPersonHardDeleteGithubOidc(token, options = {}) {
+  return verifyGitHubActionsOidcWithPolicy(token, { ...options, policy: PERSON_HARD_DELETE_POLICY });
+}
+
+module.exports = Object.freeze({
+  verifyPersonHardDeleteGithubOidc,
+  verifyPersonHardDeleteTrustClaims,
+  PERSON_HARD_DELETE_POLICY,
+  EXPECTED_AUDIENCE,
+  EXPECTED_WORKFLOW_REF
+});
