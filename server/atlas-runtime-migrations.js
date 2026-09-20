469aa5849fd0ec57161cccd5bb3be442a6f7f526
JezCH <choijanggyu@gmail.com>
2026-09-20 17:38:08 +0900

    Runtime: apply exclusion target migration

--- server/atlas-runtime-migrations.js
+++ server/atlas-runtime-migrations.js
@@ -6,7 +6,8 @@ const { assertReplaySafeCanonicalMigration } = require("./atlas-replay-migration
 
 const RUNTIME_MIGRATION_PATHS = Object.freeze([
   path.resolve(__dirname, "../db/migrations/20260906_runtime_person_politics_projection_v1.sql"),
-  path.resolve(__dirname, "../db/migrations/20260920_runtime_projection_activation_history_v1.sql")
+  path.resolve(__dirname, "../db/migrations/20260920_runtime_projection_activation_history_v1.sql"),
+  path.resolve(__dirname, "../db/migrations/20260920_runtime_compile_exclusion_targets_v1.sql")
 ]);
 
 function readRuntimeMigrations({ readFile=fs.readFileSync } = {}) {
