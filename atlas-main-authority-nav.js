63458ae0eeff73d0e3dca91fb3e1b6c74fe286ea
JezCH <choijanggyu@gmail.com>
2026-09-20 15:42:25 +0900

    Refresh visible rail name runtime cache keys

--- atlas-main-authority-nav.js
+++ atlas-main-authority-nav.js
@@ -130,7 +130,7 @@
     if (spacetimeAssetsPromise) return spacetimeAssetsPromise;
     appendStylesheetOnce("./atlas-person-spacetime-view.css?v=20260919-top-chrome-v4");
     spacetimeAssetsPromise = loadScriptOnce("./atlas-person-spacetime-model.js?v=20260903-south-asia-r3", () => Boolean(window.ATLAS_PERSON_SPACETIME_MODEL))
-      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260920-fit-minimum", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
+      .then(() => loadScriptOnce("./atlas-person-spacetime-view.js?v=20260920-visible-rail-names", () => Boolean(window.ATLAS_PERSON_SPACETIME_VIEW)))
       .then(() => window.ATLAS_PERSON_SPACETIME_VIEW)
       .catch((error) => {
         spacetimeAssetsPromise = null;
