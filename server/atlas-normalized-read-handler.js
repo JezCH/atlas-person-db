2ed820ab56d15c4504f4324c44ba1ab0e32b412e
JezCH <choijanggyu@gmail.com>
2026-09-22 01:00:35 +0900

    Read API: reuse lightweight HTTP helpers

--- server/atlas-normalized-read-handler.js
+++ server/atlas-normalized-read-handler.js
@@ -1,19 +1,7 @@
 "use strict";
 
 const { readPersonPolitics } = require("./atlas-normalized-read-service.js");
-
-function requireDatabaseUrl(env) {
-  const value = String(env?.SUPABASE_DB_URL || "").trim();
-  if (!/^postgres(?:ql)?:\/\//.test(value)) throw new Error("SUPABASE_DB_URL is required");
-  return value;
-}
-
-function sendJson(res, status, body) {
-  res.statusCode = status;
-  res.setHeader("content-type", "application/json; charset=utf-8");
-  res.setHeader("cache-control", "no-store");
-  res.end(JSON.stringify(body));
-}
+const { requireDatabaseUrl, sendJson } = require("./atlas-read-http.js");
 
 function createNormalizedReadHandler({ clientFactory, env = process.env } = {}) {
   if (typeof clientFactory !== "function") throw new Error("clientFactory is required");
